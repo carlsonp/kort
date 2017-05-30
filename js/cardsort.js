d260a1cd5ce2db9703750ad50d2f7e9480a0296d
lpberg <leif.berg@gmail.com>
2017-05-29 21:02:20 -0600

    added function to change group name by double clicksing. not sure about compatiability. updated css of group

--- js/cardsort.js
+++ js/cardsort.js
@@ -20,27 +20,35 @@ $(document).ready(function() {
 	}
 
 	createGroup = function(groupname){
-		var newElement = $('<div class="group">'+groupname+'<div class="nested accepts-items"></div></div>');
+		var newElement = $(`<div class='group'><div class='grouptitle' ondblclick="this.contentEditable=true;this.className='inEdit';" onblur="this.contentEditable=false;this.className='';" contenteditable="false" class="">`+groupname+"</div><div class='nested accepts-items'></div></div>");
 		$('#groupdrop').append(newElement);
 		updateContainers();
 	};
+
+	//add add item button
+	$('#siteHeader').append('<button id="addItemButton">Add Item</button>');
+	//add add group button
+	$('#siteHeader').append('<button id="newGroupButton">Add Group</button>');
+	//add export to JSON button
+	$('#siteHeader').append('<button id="exportJSONButton">Export JSON</button>');
+	
+	function setUpSecondaryPanels(){
+		var numPanels = Math.round($(window).width()/200);
+		for (var i = 0; i < numPanels; i++) {
+			$('body').append('<div class="column-area-secondary accepts-groups nested"></div>');
+		}
+	}
+	setUpSecondaryPanels();
 	
 	$('#newGroupButton').click(function() {
-		var groupname = prompt("Please enter group name:", "");
-		if (groupname != null || groupname != "") {
-			createGroup(groupname);
-		}
+		// var groupname = prompt("Please enter group name:", "");
+		// if (groupname != null || groupname != "") {
+		// 	createGroup(groupname);
+		// }
+		createGroup("Group");
 	});
-
-	//createGroup("Group_1");
-
-	$('#newItemButton').click(function() {
-		var itemName = prompt("Please enter item name:", "");
-		var newElement = $('<div class="item">'+itemName+'<div class="nested item-nested"></div></div>');
-		$('#initialColumn').append(newElement);
-		updateContainers();
-	});
-	$('#newBasicItemButton').click(function() {
+	
+	$('#addItemButton').click(function() {
 		var itemName = prompt("Please enter item name:", "");
 		var newElement = $('<div class="item">'+itemName+'</div>');
 		$('#initialColumn').append(newElement);
