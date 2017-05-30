3dbb6964cd80129da1deeb3631f1a475e3a015c4
lpberg <leif.berg@gmail.com>
2017-05-29 19:41:36 -0600

    added support for reading in task files

--- js/treetest.js
+++ js/treetest.js
@@ -76,23 +76,41 @@ $(document).ready(function() {
 		}
 	}
 
+	function resetTree(){
+		$('#tree').treeview('collapseAll', { silent: true });
+		$('#tree').treeview('enableAll', { silent: true });
+
+		var selectedNodes = $('#tree').treeview('getSelected');
+		selectedNodes.forEach(function(element){
+			console.log(element.nodeId)
+			$('#tree').treeview('toggleNodeSelected', [ element.nodeId, { silent: true } ]);
+		})
+		
+	}
+
 	//When node is selected, write history to console
 	$('#tree').on('nodeSelected', function(event, data) {
 		var node = $('#tree').treeview('getNode', data.nodeId);
-		console.log(getParent(node)+" | "+node.text);
+		var history = getParent(node)+" | "+node.text;
+		tasks.answers[tasks.idx] = history;
+		DOMnode = $('#taskList li').get(tasks.idx)
+		$(DOMnode).addClass("checked");
 	});
 	
 	var tasks = {
 		list: [],
+		answers: [],
 		idx: 0,
 		add: function (taskStr) {
-			this.list.push((this.list.length+1)+". "+taskStr)
+			this.list.push(taskStr);
+			this.answers.push(taskStr);
 			$('#taskList').append("<li>Task "+this.list.length+"</li>");
 		},
 		next:function() {
 			this.idx = this.idx + 1; // increase i by one
 		    this.idx = this.idx % this.list.length; // if we've gone too high, start from `0` again
 		    $('#taskDesc').html(this.list[this.idx]);
+		    resetTree();
 		},
 		prev:function() {
 		    if (this.idx === 0) { // i would become 0
@@ -103,14 +121,9 @@ $(document).ready(function() {
 		}
 	}
 
-	
-	tasks.add("Example task 1 find a new shopping bag.");
-	tasks.add("Example task 2 find a new xbox bag.");
-	tasks.add("Example task 3 find a new hourse.");
-
-
 	$("#nextTaskButton" ).click(function() {
 		tasks.next();
+		console.log(JSON.stringify(tasks));
 	});
 
 	$("#prevTaskButton" ).click(function() {
@@ -118,8 +131,33 @@ $(document).ready(function() {
 	});
 	//click on task in task list ot navigate to task
 	$('#taskList').click(function(event,data) {
-		$('#taskDesc').html(tasks.list[$( "li" ).index(event.target)]);
+		$('#taskDesc').html(tasks.list[$("li").index(event.target)]);
 	});
 
+  	function addTasksFromFile(filetext){
+	    arr = filetext.split("\n");
+	    for (var i = 0; i < arr.length; i++) {
+    		tasks.add(arr[i]);
+		}
+	}
+
+  	function readInTextFile(tasksOrTree){  
+		var file = $('#fileInput').prop('files')[0];
+		if (file.type.match(/text.*/)) {
+			var reader = new FileReader();
+			reader.onload = function() {
+				if(tasksOrTree === "tree"){
+					//do tree stuff
+				} else {
+					addTasksFromFile(reader.result);		
+				}
+			}
+			reader.readAsText(file);  
+		}
+	}
+
+	$('#fileInput').change(function(){
+		readInTextFile("tasks");
+	});
 });
 
