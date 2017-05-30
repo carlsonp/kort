726583d7b28199836960f17120d94ed24ef4b6c1
lpberg <leif.berg@gmail.com>
2017-05-29 19:42:19 -0600

    modified drake accept function to map drop and drag correctly.

--- js/cardsort.js
+++ js/cardsort.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 	var drake = dragula([].slice.apply(document.querySelectorAll('.nested')),{
 		copy: false,
-		accepts: function(el, target) {
-		  return  !el.classList.contains('no-dnd')
+		accepts: function (el, target, source, sibling) {
+			if(el.classList.contains('item') && target.classList.contains('accepts-items')){
+				return true;
+			} else if (el.classList.contains('group') && target.classList.contains('accepts-groups')){
+				return true;
+			} else {
+				return false;	
+			}
 		},
 		moves: function(el, container, handle) {
 			return  !el.classList.contains('no-dnd')
@@ -10,12 +16,12 @@ $(document).ready(function() {
 	});
 	
 	updateContainers = function(){
-			drake.containers = [].slice.apply(document.querySelectorAll('.nested'))
+		drake.containers = [].slice.apply(document.querySelectorAll('.nested'))
 	}
 
 	createGroup = function(groupname){
-		var newElement = $("<div class='group-box'><div class='groupTitle'>"+groupname+"</div><div id='"+groupname+"' class='group-content nested'></div></div>");
-		$('.content-area').append(newElement);
+		var newElement = $('<div class="group">'+groupname+'<div class="nested accepts-items"></div></div>');
+		$('#groupdrop').append(newElement);
 		updateContainers();
 	};
 	
@@ -26,18 +32,17 @@ $(document).ready(function() {
 		}
 	});
 
-	createGroup("Group_1");
-
-	$('#newColumnButton').click(function() {
-		var newElement = $('<div class="column-area nested"></div>');
-		newElement.uniqueId();
-		$('.content-area').append(newElement);
-		updateContainers();
-	});
+	//createGroup("Group_1");
 
 	$('#newItemButton').click(function() {
 		var itemName = prompt("Please enter item name:", "");
 		var newElement = $('<div class="item">'+itemName+'<div class="nested item-nested"></div></div>');
+		$('#initialColumn').append(newElement);
+		updateContainers();
+	});
+	$('#newBasicItemButton').click(function() {
+		var itemName = prompt("Please enter item name:", "");
+		var newElement = $('<div class="item">'+itemName+'</div>');
 		$('#initialColumn').append(newElement);
 		updateContainers();
 	});
