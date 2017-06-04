c47a4ba1086bebeeb4af60f7b6a20e6724eff58a
lpberg <leif.berg@gmail.com>
2017-06-03 20:42:24 -0600

    refactored and commented cardsort.js

--- js/cardsort.js
+++ js/cardsort.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	//functions for slide out menu
+	//Slide out menu
 	openMenu = function(){document.getElementById("mySidenav").style.width = "150px";}
 	closeMenu = function(){document.getElementById("mySidenav").style.width = "0px";}
 	$('#hamburger').click(function(){openMenu();});
 	$('#closeMenu').click(function(){closeMenu();});
 
+	//Dragula initialization
 	var drake = dragula([].slice.apply(document.querySelectorAll('.nested')),{
 		copy: false,
 		accepts: function (el, target, source, sibling) {
@@ -26,26 +27,23 @@ $(document).ready(function() {
 	}
 
 	deleteGroup = function(group){
-		resetItemsOfGroup(group)
-		$(group).attr("id","toBeDeleted")
-		$('#toBeDeleted').fadeOut("fast","swing",function() {$(this).remove();});
-	}
-
-	resetItemsOfGroup = function(group){
+		//return items to initial area
 		var nestedArea = group.children()[group.children().length-1]
 		var items = $(nestedArea).children()
 		items.each(function() {
 			$('#initialColumn').append(this);
 		});
+		//remove group div
+		$(group).attr("id","toBeDeleted")
+		$('#toBeDeleted').fadeOut("fast","swing",function() {$(this).remove();});
 	}
 
-	//not aplying to sub div. only parent div!
+	
 	createGroup = function(groupname){
 		var group = $("<div class='group' hidden></div>");
-		var groupTitle = $("<div class='grouptitle' contenteditable='false'>Groupname</div>")
-		var closeIcon = $("<i class='fa fa-times closeIcon' aria-hidden='true'></i>")
-		var nestedArea = $("<div class='groupArea nested accepts-items'></div>");
-
+		var groupTitle = $("<div class='title' contenteditable='false'>Groupname</div>")
+		var closeIcon = $("<i class='fa fa-times closeicon' aria-hidden='true'></i>")
+		var nestedArea = $("<div class='droparea nested accepts-items'></div>");
 		groupTitle.click(function(){
         	event.target.contentEditable=true;
 			event.target.classList.add('contenteditable')
@@ -56,7 +54,6 @@ $(document).ready(function() {
         	event.target.contentEditable=false;
 			event.target.classList.remove('contenteditable');
     	});
-
 		//pressing enter doesn't create a newline just leaves edit mode
 		groupTitle.keydown(function(event){
 		    if(event.keyCode == 13){
@@ -64,25 +61,20 @@ $(document).ready(function() {
 				event.target.classList.remove('contenteditable');
 		    }
     	});
-
 		closeIcon.click(function(event){
-        	deleteGroup($(event.target).parent());
+			var groupElement = $(event.target).parent();
+        	deleteGroup(groupElement);
     	});
 
     	group.append(closeIcon);
     	group.append(groupTitle);
     	group.append(nestedArea);
-
-		// $('#groupdrop').fadeIn("fast","swing",function() { $(this)..append(group); });
-
 		$('#groupdrop').append(group)
 		group.fadeIn();
 		updateContainers();
 	};
 
-	//add add group button
-	// $('#header').append('<button id="newGroupButton">Add Group</button>');
-
+	//setup initial drop zones for groups
 	(function (){
 		for (var i = 0; i < 3; i++) {
 			$('body').append('<div class="column-area-secondary accepts-groups nested"></div>');
@@ -92,12 +84,11 @@ $(document).ready(function() {
 	$('#newGroupButton').click(function() {createGroup("Group");});
 	
 	$('#addItem').click(function() {
-		closeMenu()
 		var itemName = prompt("Please enter item name:", "");
 		var newElement = $('<div class="item">'+itemName+'</div>');
 		$('#initialColumn').append(newElement);
 		updateContainers();
-		closeMenu()
+		closeMenu();
 	});
 
 	$('#exportJSON').click(function() {
@@ -112,6 +103,4 @@ $(document).ready(function() {
 		console.log(JSON.stringify(JSONExport));
 		closeMenu();
 	});
-
-
 });
