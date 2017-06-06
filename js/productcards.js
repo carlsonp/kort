ac1ef7ad49a44bfd79ac863ecd4cb0df5935a2ed
lpberg <leif.berg@gmail.com>
2017-06-06 17:49:39 -0600

    moved all variables and functions into a container to avoid messing up the namespace

--- js/productcards.js
+++ js/productcards.js
@@ -1,61 +1,56 @@
 $(document).ready(function() {
-
-	var words = ['Accessible', 'Desirable', 'Gets in the way', 'Patronizing', 'Stressful', 'Appealing', 'Easy to use', 'Hard to use', 'Personal', 'Time-consuming', 'Attractive', 'Efficient', 'High quality', 'Predictable', 'Time-saving', 'Busy', 'Empowering', 'Inconsistent', 'Relevant', 'Too technical', 'Collaborative', 'Exciting', 'Intimidating', 'Reliable', 'Trustworthy', 'Complex', 'Familiar', 'Inviting', 'Rigid', 'Uncontrollable', 'Comprehensive', 'Fast', 'Motivating', 'Simplistic', 'Unconventional', 'Confusing', 'Flexible', 'Not valuable', 'Slow', 'Unpredictable', 'Connected', 'Fresh', 'Organized', 'Sophisticated', 'Usable', 'Consistent', 'Frustrating', 'Overbearing', 'Stimulating', 'Useful', 'Customizable', 'Fun', 'Overwhelming', 'Straight Forward', 'Valuable'];
-	var wordSelectionLimit = 5;
-	var onStageTwo = false;
-	var selectedArr = [];
-	var selectedNum = 0;
-	$('#allCardsSelected').hide();
-
-	function updateView(){
-		if(selectedNum === wordSelectionLimit){
-			$( "#cardArea li:not(.selected)").toggleClass('disabled');
-			$('#textArea').html('All Done!');
-			recordSelected();
-		} else {
-			$( "#cardArea li").removeClass('disabled');
-			$('#textArea').html('Select your top 5 cards.');
+	var c = {
+		words: ['Accessible', 'Desirable', 'Gets in the way', 'Patronizing', 'Stressful', 'Appealing', 'Easy to use', 'Hard to use', 'Personal', 'Time-consuming', 'Attractive', 'Efficient', 'High quality', 'Predictable', 'Time-saving', 'Busy', 'Empowering', 'Inconsistent', 'Relevant', 'Too technical', 'Collaborative', 'Exciting', 'Intimidating', 'Reliable', 'Trustworthy', 'Complex', 'Familiar', 'Inviting', 'Rigid', 'Uncontrollable', 'Comprehensive', 'Fast', 'Motivating', 'Simplistic', 'Unconventional', 'Confusing', 'Flexible', 'Not valuable', 'Slow', 'Unpredictable', 'Connected', 'Fresh', 'Organized', 'Sophisticated', 'Usable', 'Consistent', 'Frustrating', 'Overbearing', 'Stimulating', 'Useful', 'Customizable', 'Fun', 'Overwhelming', 'Straight Forward', 'Valuable'],
+		wordSelectionLimit: 5,
+		onStageTwo: false,
+		selectedArr: [],
+		selectedNum: 0,
+		init: function(){
+			$('#next').hide();
+			this.addWords();
+			this.bindNextButton();
+			this.bindCardClick();
+		},
+		addWords: function(){
+			for (var i = 0; i < this.words.length; i++) {
+				$('#cardArea').append("<li>"+this.words[i]+"</li>");
+			}
+		},
+		bindNextButton: function(){
+		  	$("#next").click(function() {
+				$( "#cardArea li:not(.selected)").each(function( index ) {
+					$(this).remove();
+				});
+				$("#cardArea li.selected").toggleClass('selected');
+				c.onStageTwo = true;
+				c.updateView();
+				$(this).remove();
+			});
+		},
+		bindCardClick: function(){
+	  		$("#cardArea li").click(function() {
+				$(this).toggleClass("selected");
+				c.updateView();
+			});
+		},
+		updateView: function(){
+			this.selectedNum = $('#cardArea li.selected').length;
+			if(this.onStageTwo){
+				if(this.selectedNum === this.wordSelectionLimit){
+					$( "#cardArea li:not(.selected)").toggleClass('disabled');
+					$('#textArea').html('All Done!');
+					printSelectedToConsole();
+				} else {
+					$( "#cardArea li").removeClass('disabled');
+					$('#textArea').html('Select your top 5 cards.');
+				}
+			} else {
+				$('#textArea').html('Select at least 10 cards that represent your experience with the system. ('+this.selectedNum+' selected)');
+				if(this.selectedNum > 9){$('#next').show();} 
+				else {$('#next').hide();}
+			}
 		}
 	}
-
-	function recordSelected(){
-		selectedArr = [];
-		$( "#cardArea li.selected").each(function( index ) {
-		  selectedArr.push($(this).text());
-		});
-		console.log(selectedArr);
-	}
-
-	function addWords(){
-		for (var i = 0; i < words.length; i++) {
-			var newWord = $("<li>"+words[i]+"</li>");
-			$('#cardArea').append(newWord);
-		}
-	}
-	addWords();
-
-  	$("#cardArea li").click(function() {
-		$(this).toggleClass("selected");
-		selectedNum = $('#cardArea li.selected').length;
-		if(onStageTwo){
-			updateView();	
-		} else {
-			$('#textArea').html('Select at least 10 cards that represent your experience with the system. ('+selectedNum+' selected)');
-			if(selectedNum > 9){$('#allCardsSelected').show();} 
-			else {$('#allCardsSelected').hide();}
-		}
-	});
-
-  	$("#allCardsSelected").click(function() {
-		$( "#cardArea li:not(.selected)").each(function( index ) {
-			$(this).remove();
-		});
-		$('#textArea').html('Select your top 5 cards.');
-		$( "#cardArea li.selected").toggleClass('selected');
-		onStageTwo = true;
-		$(this).remove();
-	});
-
-	//-------------------------------------------------------------------
+	c.init();
 });
 
