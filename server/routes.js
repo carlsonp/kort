11599a9005add2c16f1ff688143b791db8888be7
Patrick Carlson <carlson2442@gmail.com>
2017-06-18 15:41:54 -0600

    update routes to require authentication before access

--- server/routes.js
+++ server/routes.js
@@ -5,68 +5,61 @@ var treetest = require('./treetest_server');
 var productreaction = require('./productreaction_server');
 
 
-module.exports = function(app, passport) {
+module.exports = function(app, passport, flash) {
 
 	app.get('/', function (req, res) {
 		res.render('index.ejs');
 	});
 
-	app.get('/productcards', function (req, res) {
-		res.render('productcards.ejs');
-	});
-
-	app.get('/treetest', function (req, res) {
-		res.render('treetest.ejs');
-	});
-
-	app.post('/createcardsort', cardsort.create);
-	app.get('/editcardsort/:id', cardsort.edit);
-	app.get('/deletecardsort/:id', cardsort.delete);
+	app.post('/createcardsort', isLoggedIn, cardsort.create);
+	app.get('/editcardsort/:id', isLoggedIn, cardsort.edit);
+	app.get('/deletecardsort/:id', isLoggedIn, cardsort.delete);
 	app.get('/cardsort/:id', cardsort.view);
-	app.get('/cardsortresults/:id', cardsort.results);
-	app.post('/updatecardsort', cardsort.update);
+	app.get('/cardsortresults/:id', isLoggedIn, cardsort.results);
+	app.post('/updatecardsort', isLoggedIn, cardsort.update);
 
-	app.post('/createtreetest', treetest.create);
-	app.get('/edittreetest/:id', treetest.edit);
-	app.get('/deletetreetest/:id', treetest.delete);
+	app.post('/createtreetest', isLoggedIn, treetest.create);
+	app.get('/edittreetest/:id', isLoggedIn, treetest.edit);
+	app.get('/deletetreetest/:id', isLoggedIn, treetest.delete);
 	app.get('/treetest/:id', treetest.view);
-	app.get('/treetestresults/:id', treetest.results);
-	app.post('/updatetreetest', treetest.update);
+	app.get('/treetestresults/:id', isLoggedIn, treetest.results);
+	app.post('/updatetreetest', isLoggedIn, treetest.update);
 
-	app.post('/createproductreaction', productreaction.create);
-	app.get('/editproductreaction/:id', productreaction.edit);
-	app.get('/deleteproductreaction/:id', productreaction.delete);
+	app.post('/createproductreaction', isLoggedIn, productreaction.create);
+	app.get('/editproductreaction/:id', isLoggedIn, productreaction.edit);
+	app.get('/deleteproductreaction/:id', isLoggedIn, productreaction.delete);
 	app.get('/productreaction/:id', productreaction.view);
-	app.get('/productreactionresults/:id', productreaction.results);
-	app.post('/updateproductreaction', productreaction.update);
+	app.get('/productreactionresults/:id', isLoggedIn, productreaction.results);
+	app.post('/updateproductreaction', isLoggedIn, productreaction.update);
 
-	app.get('/admin', function (req, res) {
-		res.render('admin.ejs');
+	app.get('/admin', isLoggedIn, function (req, res) {
+		res.render('admin.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
 	});
 
-	app.get('/studies', admin.Studies);
+	app.get('/studies', isLoggedIn, admin.Studies);
 
+	app.get('/usermanagement', isLoggedIn, admin.UserManagement);
 
 	app.get('/login', function(req, res) {
-		res.render('login.ejs');
+		res.render('login.ejs', { message: req.flash('loginMessage') });
 	});
 	  
-	app.post('/login', passport.authenticate('local', { failureRedirect: '/login/fail' }), function(req, res) {
-		res.redirect('/');
-	});
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/admin',
+		failureRedirect: '/login',
+		failureFlash : true
+	}));
 	  
 	app.get('/logout', function(req, res) {
 		req.logout();
 		res.redirect('/');
 	});
 	
-	app.get('/createuser', function(req, res) {
-		res.render('createuser.ejs');
-	});
-	
-	app.post('/createuser', passport.authenticate('local-signup', {
-		successRedirect : '/profile',
-		failureRedirect : '/createuser',
+	app.post('/createuser', isLoggedIn, passport.authenticate('local-signup', {
+		successRedirect : '/usermanagement',
+		failureRedirect : '/usermanagement',
 		failureFlash : true
 	}));
 }
@@ -78,5 +71,5 @@ function isLoggedIn(req, res, next) {
         return next();
 
     // if they aren't redirect them to the home page
-    res.redirect('/');
+    res.redirect('/login');
 }
