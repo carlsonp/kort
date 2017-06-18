8fc0f64c19de8a119070ec9d6e906787550b67ca
lpberg <leif.berg@gmail.com>
2017-06-18 11:58:36 -0600

    updated Schemas

--- models/study.js
+++ models/study.js
@@ -21,11 +21,21 @@ var TreeTestStudy = new mongoose.Schema({
 	type: String,
 	tasks: [],
     tree: [],
+    responses: [{
+		participantID: String,
+		tasks: [],
+		paths: [],
+	}]
 });
 
 var ProductReactionStudy = new mongoose.Schema({
 	title: String,
 	type: String,
+	words: [],
+	responses: [{
+		participantID: String,
+		words: [],
+	}]
 });
 
 module.exports = mongoose.model('CardSortStudy', CardSortStudy);
