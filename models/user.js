855f87f051c9229cc9c5ed570a1b4042a1b14de5
Patrick Carlson <carlson2442@gmail.com>
2017-06-18 09:36:42 -0600

    add user schema for login

---
+++ models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var User = new mongoose.Schema({
+	name: String,
+	password: String,
+	email: String,
+	studies: []
+});
+
+
+// generating a hash
+User.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+User.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+
+module.exports = mongoose.model('User', User);
