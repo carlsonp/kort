887adacb8c32de8b6c909fbedcf48f62659e4d43
Patrick Carlson <carlson2442@gmail.com>
2017-06-18 15:41:41 -0600

    setup passport strategy for login

--- server/passport.js
+++ server/passport.js
@@ -1,8 +1,7 @@
 require('mongoose').model('User');
 var mongoose = require('mongoose');
 
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var db = require('./db');
 
 var User = mongoose.model('User');
@@ -27,9 +26,10 @@ module.exports = function(passport) {
         // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
         passwordField : 'password',
+		name : 'name',
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
-    function(req, email, password, done) {
+    function(req, email, password, name, done) {
 
         // asynchronous
         // User.findOne wont fire unless data is sent back
@@ -44,7 +44,7 @@ module.exports = function(passport) {
 
 				// check to see if theres already a user with that email
 				if (user) {
-					return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+					return done(null, false, req.flash('createUserMessage', 'That email is already taken.'));
 				} else {
 
 					// if there is no user with that email, create new user
@@ -53,15 +53,44 @@ module.exports = function(passport) {
 					// set the user's local credentials
 					newUser.email = email;
 					newUser.password = newUser.generateHash(password);
+					newUser.name = name;
 
 					// save the user
 					newUser.save(function(err) {
 						if (err)
 							throw err;
-						return done(null, newUser);
+						return done(null, newUser, req.flash('createUserMessage', 'Account created successfully.'));
 					});
 				}
 			});    
         });
     }));
+	
+	passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'email' :  email }, function(err, user) {
+            // if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
+
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+            // if the user is found but the password is wrong
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+            // all is well, return successful user
+            return done(null, user);
+        });
+	}));
 };
