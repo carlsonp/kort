107b2d7a7799158da74feb8ad1324d445d28f50c
Patrick Carlson <carlson2442@gmail.com>
2017-06-20 20:32:06 -0600

    use appropriate new function

--- server/routes.js
+++ server/routes.js
@@ -40,7 +40,7 @@ module.exports = function(app, passport, flash) {
 
 	app.get('/studies', isLoggedIn, admin.Studies);
 
-	app.get('/usermanagement', isLoggedIn, admin.UserManagement);
+	app.get('/usermanagement', isLoggedIn, user.UserManagement);
 
 	app.get('/login', function(req, res) {
 		res.render('login.ejs', { message: req.flash('loginMessage') });
