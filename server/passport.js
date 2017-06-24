4c1a5526847bc415f415f1e61cbe06d2d8ea947f
lpberg <leif.berg@gmail.com>
2017-06-24 06:19:00 -0600

    removed console.log statement and spacing

--- server/passport.js
+++ server/passport.js
@@ -8,20 +8,16 @@ var User = mongoose.model('User');
 
 
 module.exports = function(passport, flash) {
-
 	// used to serialize the user for the session
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
-
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user) {
             done(err, user);
         });
     });
-
-
     passport.use('local-signup', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
@@ -29,21 +25,16 @@ module.exports = function(passport, flash) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) {
-
         // asynchronous
         // User.findOne wont fire unless data is sent back
-        console.log("d"+password);
         process.nextTick(function() {
-
 			// find a user whose email is the same as the forms email
 			// we are checking to see if the user trying to login already exists
 			User.findOne({ 'email' :  email }, function(err, user) {
 				// if there are any errors, return the error
 				if (err)
 					return done(err);
-
 				// check to see if theres already a user with that email
-                
 				if (user) {
 					return done(null, false, req.flash('createUserErrorMessage', 'Email is already taken.'));
 				} else if (password.length < 1) {
@@ -51,14 +42,11 @@ module.exports = function(passport, flash) {
 				} else if (email.length < 0) {
 					return done(null, false, req.flash('createUserErrorMessage', 'Email field is blank.'));
 				} else {
-
 					// if there is no user with that email, create new user
 					var newUser = new User();
-
 					// set the user's local credentials
 					newUser.email = email;
 					newUser.password = newUser.generateHash(password);
-
 					// save the user
 					newUser.save(function(err) {
 						if (err)
