a345e6c4575c8d7e31c0d10bcd00327d09da3b49
lpberg <leif.berg@gmail.com>
2017-07-03 20:13:08 -0600

    added bar graph to product reactions resuls page.

--- server/productreaction_server.js
+++ server/productreaction_server.js
@@ -83,13 +83,32 @@ module.exports = {
         });
     },
     results: function (req, res, next) {
-        ProductReactionStudy.findOne({_id: req.params.id}, function (err, docs) {
+        ProductReactionStudy.findOne({_id: req.params.id}, function (err, study) {
             if (err) {
                 res.status(504);
                 console.log("productreaction_server.js: Error getting study to see results.");
                 res.end(err);
             } else {
-                res.render('desirability/results.ejs',{study: docs});
+                //gather all words from all responses and put into single array
+                var allWords = []
+                for (var i = 0; i < study.responses.length; i++) {
+                    var response = study.responses[i]
+                    for (var j = 0; j < response.length-1; j++) {
+                        allWords.push(response[j])
+                    }
+                }
+                var combined = allWords.reduce(function (acc, curr) {
+                  if (typeof acc[curr] == 'undefined') {acc[curr] = 1;} 
+                  else {acc[curr] += 1;}
+                  return acc;
+                }, {});
+                var words = Object.keys(combined);
+                var counts = [];
+                for (var i = 0; i < words.length; i++) {
+                    counts.push(combined[words[i]]);
+                }
+
+                res.render('desirability/results.ejs',{study: study, words: words, counts: counts});
             }
         });
     },
