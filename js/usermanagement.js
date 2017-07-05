d8c0b5f4f4489905263d2479cc596ba3dc06fd63
lpberg <leif.berg@gmail.com>
2017-07-05 17:37:42 -0600

    moved js functions into their own js files for usermanagement and index

---
+++ js/usermanagement.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    $( ".successMessage" ).slideDown(300).delay(2000).slideUp(300);
+    $( ".errorMessage" ).slideDown(300);
+});
+function confirmDelete(href,username){
+	 bootbox.confirm({
+    	size: 'small',
+    	closeButton: false,
+	    message: "Delete "+username+"? This will also delete any studies created by this user.",
+	    buttons: {confirm: {label: 'Delete',className: 'btn-danger'},
+        		  cancel: {label: 'Cancel',className: 'btn-link'}
+	    },
+	    callback: function (result) {
+	    	if(result){
+	    		window.location.href = href
+	    	}
+	    }
+	});
+}
