2c53df2eff518efcb560d837905e0ceb38850308
lpberg <leif.berg@gmail.com>
2017-07-23 16:19:24 -0600

    cremoved unused attributes for createResponse function

--- server/response.js
+++ server/response.js
@@ -1,23 +1,17 @@
-// require('mongoose').model('Study');
-// require('mongoose').model('Response');
 var mongoose = require('mongoose');
 var Response = mongoose.model('Response');
-
 
 module.exports = {
 	createResponse: function(studyID, title){
 		var response = new Response({
 			title: title,
 		    studyID: studyID,
-		    //date: new Date(Date.now()),
 		    data: [],
-		    data_temp: [],
 		    complete: false,
 		});
 		response.save(function (err) {
 		    if (err) return handleError(err);
 		});
-
 		return response;
 	},
 }
