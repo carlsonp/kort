36e5914184e7b98ff4b4d12d81c557f509074db4
Leif Berg <leif.berg@gmail.com>
2017-08-06 12:52:51 -0600

    added ability to add cards and groups from a list

--- js/cardsort_edit.js
+++ js/cardsort_edit.js
@@ -9,6 +9,71 @@ function updateInputArrays(){
 	$('#groups').val(JSON.stringify(groups_table.column(0).data().toArray()));
 }
 
+function createCard(str){
+	if (!str == '') {
+		var deleteLink = "<td><a class='text-danger' href='#' onclick='deleteCard(this)'>Delete</a></td>";
+		cards_table.row.add([str,deleteLink]).draw(false);
+		$('#newCardInput').val('');
+		updateInputArrays();
+	}
+
+}
+
+function createGroup(str){
+	if (!str == '') {
+		var deleteLink = "<td><a class='text-danger' href='#' onclick='deleteCard(this)'>Delete</a></td>";
+		groups_table.row.add([str,deleteLink]).draw(false);
+		$('#newGroupInput').val('');
+		updateInputArrays();
+}
+
+function addCardsFromDialog(){
+	bootbox.prompt({
+		title: "<h3>Add from List</h3><p>Enter a list of cards names (one card per line).</p>",
+		inputType: 'textarea',
+		closeButton: false,
+		message: "Add many cards at once. Enter a list of cards with one per line.",
+	    buttons: {confirm: {label: 'Add',className: 'btn-success'},
+        		  cancel: {label: 'Cancel',className: 'btn-link'}
+	    },
+		callback: function (result) {
+			if(result){
+				var cards = result.split(/\r?\n/).map(function(item) {
+              		return item.trim();
+				}).filter(function(n){ return n != '' });
+
+				for (var i = 0; i < cards.length; i++) {
+					createCard(cards[i]);
+				}
+			}
+		}
+	});
+}
+
+function addGroupsFromDialog(){
+	bootbox.prompt({
+		title: "<h3>Add from List</h3><p>Enter a list of group names (one per line).</p>",
+		inputType: 'textarea',
+		closeButton: false,
+		message: "Add many groups at once. Enter a list of groups with one card per line.",
+	    buttons: {confirm: {label: 'Add',className: 'btn-success'},
+        		  cancel: {label: 'Cancel',className: 'btn-link'}
+	    },
+		callback: function (result) {
+			if(result){
+				var groups = result.split(/\r?\n/).map(function(item) {
+              		return item.trim();
+				}).filter(function(n){ return n != '' });
+
+				for (var i = 0; i < groups.length; i++) {
+					createGroup(groups[i]);
+				}
+			}
+		}
+	});
+}
+
+
 //removes a list item (card) from the cards_table datatable
 function deleteCard(target){
 	var row = $(target).parent().parent();
@@ -22,6 +87,8 @@ function deleteGroup(target){
 	groups_table.row(row).remove().draw(false);
 	updateInputArrays();
 }
+
+
 
 $( document ).ready(function() {
 	//toggles input area for groups based on studyType (open or closed)
@@ -49,10 +116,7 @@ $( document ).ready(function() {
 
 	//event listener for adding cards to the cards_table datatable
 	$('#addCardBtn').click(function(event){
-		var deleteLink = "<td><a class='text-danger' href='#' onclick='deleteCard(this)'>Delete</a></td>";
-		cards_table.row.add([$('#newCardInput').val().trim(),deleteLink]).draw(false);
-		$('#newCardInput').val('');
-		updateInputArrays();
+		createCard($('#newCardInput').val().trim())
 	});
 
 	//initalize groups_table DataTable
@@ -70,10 +134,7 @@ $( document ).ready(function() {
 
 	//event listender for adding groups to the groups_table datatable
 	$('#addGroupBtn').click(function(event){
-		var deleteLink = "<td><a class='text-danger' href='#' onclick='deleteGroup(this)'>Delete</a></td>";
-		groups_table.row.add([$('#newGroupInput').val().trim(),deleteLink]).draw(false);
-		$('#newGroupInput').val('');
-		updateInputArrays();
+		createGroup($('#newGroupInput').val().trim());
 	});
 
 	//places existing cards and groups into input fields (so save works when no changes are made)
