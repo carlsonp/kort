254f39c12c57c7ee84e0c85a72c90aecff929760
lpberg <leif.berg@gmail.com>
2017-08-20 07:48:19 -0600

    changed private.js functions to be more secure (moved into event handlers)

--- js/private.js
+++ js/private.js
@@ -11,7 +11,25 @@ $( document ).ready(function() {
  		var url = $(this).data("url");
  		var title = $('#newResponseInput').val();
  		var studyType = $(this).data("studytype");
- 		createResponse(studyID,url,title,studyType);
+ 		if (title == '') {
+ 			title = "Anonymous";
+ 		}
+		$.ajax({
+	        url: '/createresponse_ajax/'+studyID,
+	        type: "POST",
+	        data: {title: title},
+	        success: function(response) {
+	          	$(`<tr>
+	          			<td>`+response.title+`</td>
+						<td><a href='`+url+`/`+studyType+`/`+studyID+`/`+response._id+`'>`+url+`/`+studyType+`/`+studyID+`/`+response._id+`</a></td>
+						<td><a href="" class="copyText" data-placement="right" data-clipboard-text="`+url+`/`+studyType+`/`+studyID+`/`+response._id+`" data-toggle="tooltip" title="Copied!">Copy</a></td>
+						<td><a class='text-danger' data-studyid="`+studyID+`" data-resid="`+response._id+`" href="">Delete</a></td>
+					</tr>`).appendTo($('#responses_table_body'));
+	        },
+	        error:   function(xhr, text, err) {
+	      		console.log("Response: Please check ajax request");
+	        }
+	  	});
  		$('#newResponseInput').val("");
  	});
 
@@ -31,55 +49,31 @@ $( document ).ready(function() {
 	    event.preventDefault();
 	    var studyID = $(this).data("studyid");
 	    var resid = $(this).data("resid");
-	    deleteResponse(studyID,resid,this);
+	    var parentRow = $(this).parent().parent()
+	    bootbox.confirm({
+	    	size: 'small',
+	    	closeButton: false,
+		    message: "<b>Delete Response?</b><br>This cannot be undone.",
+		    buttons: {confirm: {label: 'Delete Response',className: 'btn-danger'},
+	        		  cancel: {label: 'Cancel',className: 'btn-link'}
+		    },
+		    callback: function (result) {
+		    	if(result){
+		    		$.ajax({
+						url:  "/deleteresponse/"+studyID+"/"+resid,
+						type: "POST",
+						contentType: "application/json",
+						success: function(data) {
+		  	    			parentRow.remove()
+						},
+						error:   function(xhr, text, err) {
+						  console.log("private.ejs: delete Response ajax error");
+						}
+					});
+		    	}
+		    }
+		});
 	});
-
-
  });
 
-function deleteResponse(studyID,responseID,target){
-	bootbox.confirm({
-    	size: 'small',
-    	closeButton: false,
-	    message: "<b>Delete Response?</b><br>This cannot be undone.",
-	    buttons: {confirm: {label: 'Delete Response',className: 'btn-danger'},
-        		  cancel: {label: 'Cancel',className: 'btn-link'}
-	    },
-	    callback: function (result) {
-	    	if(result){
-	    		$.ajax({
-					url:  "/deleteresponse/"+studyID+"/"+responseID,
-					type: "POST",
-					contentType: "application/json",
-					success: function(data) {
-	  	    			var tableRowToDelete = $(target).parent().parent()
-	    				tableRowToDelete.remove();
-					},
-					error:   function(xhr, text, err) {
-					  console.log("private.ejs: delete Response ajax error");
-					}
-				});
-	    	}
-	    }
-	});
-}
 
-function createResponse(studyID,url,title,studyType){
-	if (title == '') title = "Anonymous";
-	$.ajax({
-        url: '/createresponse_ajax/'+studyID,
-        type: "POST",
-        data: {title: title},
-        success: function(response) {
-          	$(`<tr>
-          			<td>`+response.title+`</td>
-					<td><a href='`+url+`/`+studyType+`/`+studyID+`/`+response._id+`'>`+url+`/`+studyType+`/`+studyID+`/`+response._id+`</a></td>
-					<td><a href="" class="copyText" data-placement="right" data-clipboard-text="`+url+`/`+studyType+`/`+studyID+`/`+response._id+`" data-toggle="tooltip" title="Copied!">Copy</a></td>
-					<td><a class='text-danger' data-studyid="`+studyID+`" data-resid="`+response._id+`" href="">Delete</a></td>
-				</tr>`).appendTo($('#responses_table_body'));
-        },
-        error:   function(xhr, text, err) {
-      		console.log("Response: Please check ajax request");
-        }
-  	});
-}
