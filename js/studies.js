0515da4d95f4df1b21ecc1cd7d7cb31cc097467c
lpberg <leif.berg@gmail.com>
2017-08-25 20:15:35 -0600

    clarified status on studies page to be consistent with edit page

--- js/studies.js
+++ js/studies.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
 	$('td[data-type="cardsort"]').html('Card Sort');
 	$('td[data-type="treetest"]').html('Tree Test');
 	$('td[data-type="productreactioncards"]').html('Product Reaction Cards');
+	$('td[data-status="open"]').html('Accepting Responses');
+	$('td[data-status="closed"]').html('Not Accepting Responses');
 
 	studies_table = $('#studies_table').DataTable({
 		"paging":   false,
