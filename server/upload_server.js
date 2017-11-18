ff3a1a144ae775630d80db3c5405fbf67dc4c445
Leif Berg <leif.berg@gmail.com>
2017-11-18 10:11:55 -0600

    fixed issue with "race condition" when deleting uploads

--- server/upload_server.js
+++ server/upload_server.js
@@ -7,14 +7,13 @@ module.exports = {
     create: function(req,res){
         if(req.file) {
             var upload = new Upload({
-                    title: req.file.filename,
-                    path: req.file.path,
+                title: req.file.filename,
+                path: req.file.path,
             });
             var filepath = path.parse(req.file.path);
-            var newFileName = filepath.dir+filepath.sep+upload._id+filepath.ext;
+            var newFileName = filepath.dir+path.sep+upload._id+filepath.ext;
             fs.rename(req.file.path, newFileName, function (err) {
-              if (err) throw err;
-              console.log('upload_server.js: rename complete');
+                if (err) throw err;
             });
             upload.path = newFileName;
             upload.save(function (err) {
@@ -37,24 +36,28 @@ module.exports = {
                 console.log(err);
                 req.end();
             } else {
+                //if file (not db doc) exists remove it
                 fs.stat(doc.path, function (err, stats) {
                    if (err) {
                        return console.error(err);
                    }
                    fs.unlink(doc.path,function(err){
-                        if(err) return console.log(err);
-                        console.log(doc.path+' deleted successfully');
-                   });  
+                        if (err) {
+                            return console.log(err);
+                        } else {
+                            //remove document from collection (not file)
+                            doc.remove(function (err) {
+                                if (err) {
+                                    console.log(err);
+                                    res.end(err);
+                                } else {
+                                    res.send(true);
+                                    res.end();
+                                }
+                            });
+                        } 
+                    });  
                 });
-            }
-        }).remove(function (err) {
-            if (err) {
-                console.log(err);
-                res.end(err);
-            } else {
-                console.log("document removed from db");
-                res.send(true);
-                res.end();
             }
         });
     },
