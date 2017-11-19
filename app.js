36e029ac6ae0e1b01b6f91762fb4984a4cf67324
Patrick Carlson <carlson2442@gmail.com>
2017-11-19 10:40:32 -0700

    use open-sans font from NPM package

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(flash());
 //https://stackoverflow.com/questions/14264429/how-to-use-jquery-installed-with-npm-in-express-app
 //https://expressjs.com/en/starter/static-files.html
 app.use('/css', express.static(__dirname + '/css/'));
+app.use('/opensans', express.static(__dirname + '/node_modules/npm-font-open-sans/'));
 app.use('/uploads', express.static(__dirname + '/uploads/'));
 app.use('/js', express.static(__dirname + '/js/'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
