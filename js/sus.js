f48a301c6b2676f806e26dcc3893b7af96257c4e
Leif Berg <leif.berg@gmail.com>
2017-11-23 09:41:41 -0600

    cleaned up sus submission values

--- js/sus.js
+++ js/sus.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
 	$('#submitForm').click(function(event) {
 		event.preventDefault();
-		var result = [0,0,0,0,0,0,0,0,0,0];
-  		// result = [$('input[name=1]:checked').val(),
-  		// 		  $('input[name=2]:checked').val(),
-  		// 		  $('input[name=3]:checked').val(),
-  		// 		  $('input[name=4]:checked').val(),
-  		// 		  $('input[name=5]:checked').val(),
-  		// 		  $('input[name=6]:checked').val(),
-  		// 		  $('input[name=7]:checked').val(),
-  		// 		  $('input[name=8]:checked').val(),
-  		// 		  $('input[name=9]:checked').val(),
-  		// 		  $('input[name=10]:checked').val()];
-	  	console.log(result);
+		var result = [$('input[name=1]:checked').val(),
+				  $('input[name=2]:checked').val(),
+				  $('input[name=3]:checked').val(),
+				  $('input[name=4]:checked').val(),
+				  $('input[name=5]:checked').val(),
+				  $('input[name=6]:checked').val(),
+				  $('input[name=7]:checked').val(),
+				  $('input[name=8]:checked').val(),
+				  $('input[name=9]:checked').val(),
+				  $('input[name=10]:checked').val()
+        ];
 	   $('#hiddenResults').val(JSON.stringify(result));
 	   $('#susForm').submit()
 	});
