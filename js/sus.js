580e7f593ad36347ef57e7c6d2c021b67208dde9
Leif Berg <leif.berg@gmail.com>
2017-11-23 12:37:33 -0600

    added simple validation - no user messages yet, submit button is diabled until all responses are collected.

--- js/sus.js
+++ js/sus.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#submitForm').click(function(event) {
 		event.preventDefault();
-		var result = [$('input[name=1]:checked').val(),
+		var result = [$('input[name=1]:checked').val()&&
 				  $('input[name=2]:checked').val(),
 				  $('input[name=3]:checked').val(),
 				  $('input[name=4]:checked').val(),
@@ -12,8 +12,33 @@ $(document).ready(function() {
 				  $('input[name=9]:checked').val(),
 				  $('input[name=10]:checked').val()
         ];
-	   $('#hiddenResults').val(JSON.stringify(result));
-	   $('#susForm').submit()
+        var completed = true;
+        for (var i = 0; i < result.length; i++) {
+        	if (result[i] == null) completed = false;
+        }
+        if (completed){
+        	$('#hiddenResults').val(JSON.stringify(result));
+	   		$('#susForm').submit();
+        }
+	   	
+	});
+	// once all items have answers, enable the submit button
+	// this is one way to do validation
+	$("input").change(function(){
+    	if ($('input[name=1]:checked').val() &&
+		  $('input[name=2]:checked').val()&&
+		  $('input[name=3]:checked').val()&&
+		  $('input[name=4]:checked').val()&&
+		  $('input[name=5]:checked').val()&&
+		  $('input[name=6]:checked').val()&&
+		  $('input[name=7]:checked').val()&&
+		  $('input[name=8]:checked').val()&&
+		  $('input[name=9]:checked').val()&&
+		  $('input[name=10]:checked').val()) {
+    		$('#submitForm').removeClass("disabled");
+    	} else {
+    		$('#submitForm').addClass("disabled");
+    	}
 	});
 });
 
