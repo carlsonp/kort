85a5795306fd667941ba3d3c4431530e0db5df25
lpberg <leif.berg@gmail.com>
2017-11-29 19:13:52 -0700

    fixed issue with product reaction cards not submitting during preview

--- js/productreactioncards.js
+++ js/productreactioncards.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	pc = {
+	var pc = {
 		secondSelectionLimit: 5,
 		firstSelectionLimit: 10,
 		onStageTwo: false,
@@ -8,32 +8,28 @@ $(document).ready(function() {
 
 	function shuffle(array) {
 	  var currentIndex = array.length, temporaryValue, randomIndex;
-
 	  // While there remain elements to shuffle...
 	  while (0 !== currentIndex) {
-
 	    // Pick a remaining element...
 	    randomIndex = Math.floor(Math.random() * currentIndex);
 	    currentIndex -= 1;
-
 	    // And swap it with the current element.
 	    temporaryValue = array[currentIndex];
 	    array[currentIndex] = array[randomIndex];
 	    array[randomIndex] = temporaryValue;
 	  }
-
 	  return array;
 	}
 
 	function loadDatafromDB(){
 		pc.words = $('#hiddenWords').val().split(";").map(function(item) {
 			  return item.trim();
-		}).filter(function(n) {
-			return n != ''
-		});
+		}).filter(function(n) { return n != ''});
+
 		if ($('#hiddenRandomize').val() == "on") {
 			pc.words = shuffle(pc.words)
 		}
+
 		pc.responseID = $('#resid').val();
 		
 		for (var i = 0; i < pc.words.length; i++) {
@@ -43,19 +39,21 @@ $(document).ready(function() {
 		if($('#hiddenStatus').val() == 'open'){
 			$("#done").click(function() {
 				if ($("#cardArea li.selected").length >= pc.secondSelectionLimit) {
-						pc.results = [];
-						$('#cardArea li.selected').each(function(idx,word){
-							pc.results.push($(word).text());
-						});
-						$('#hiddenResults').val(JSON.stringify(pc.results));
-						$('#submitForm').click();
-					}
-				});
+					pc.results = [];
+					$('#cardArea li.selected').each(function(idx,word){
+						pc.results.push($(word).text());
+					});
+					$('#hiddenResults').val(JSON.stringify(pc.results));
+					console.log('here')
+					$('#submitForm').click();
+				}
+			});	
 		}
 		$('#hiddenWords').remove();
 		$('#hiddenStatus').remove();
 		$('#hiddenRandomize').remove();
 	}
+
 	function enableButton(buttonID){
 		$(buttonID).removeClass('disabled');
 		$(buttonID).addClass('btn-amber');
