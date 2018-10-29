bb7747f9a40ef53a799e99fdd8350dfd9be34f6f
Leif Berg <leif.berg@gmail.com>
2018-10-28 20:19:54 -0600

    Ejs data refactor (#228)
    * began refactoring ejs data 'injection' from hidden input fields to using a consistent function call from javascript
    
    * refactored product reaction cards js

--- js/cardsort.js
+++ js/cardsort.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
-	var cs = {};
-	cs.groupNum = 0;
-	cs.zoneNum = 5;
+var cs = {};
+cs.groupNum = 0;
+cs.zoneNum = 5;
+var drake = null;
 
-	function updateGroupArray(){
-		cs.groups = [];
-		$('.group').each(function(index) {
-			var title = $(this).children('.title')
-			cs.groups.push(title.text());
-		});
-	}
-	
+function updateGroupArray(){
+	cs.groups = [];
+	$('.group').each(function(index) {
+		var title = $(this).children('.title')
+		cs.groups.push(title.text());
+	});
+}
+
+function initDrake() {
 	var drake = dragula([].slice.apply(document.querySelectorAll('.nested')),{
 		copy: false,
 		accepts: function (el, target, source, sibling) {
@@ -26,166 +27,165 @@ $(document).ready(function() {
 			return  !el.classList.contains('no-dnd')
 		},
 	});
+	return drake;
 
-	function groupNameExists(groupname){
-		if (cs.groups && cs.groups.indexOf(groupname) !== -1){
-			return true;
-		} else {
-			return false;
-		}
+}
+
+function groupNameExists(groupname){
+	if (cs.groups && cs.groups.indexOf(groupname) !== -1){
+		return true;
+	} else {
+		return false;
 	}
+}
 
-	function updateContainers(){
-		drake.containers = [].slice.apply(document.querySelectorAll('.nested'))
-	}
+function updateContainers(){
+	drake.containers = [].slice.apply(document.querySelectorAll('.nested'))
+}
 
-	function deleteGroup(group){
-		cs.groupNum-=1;
-		//get droparea div and put all items back to init column
-		$(group).children('.droparea').children().each(function() {
-			$('#initialColumn').append(this);
-		});
-		//remove group div
-		$(group).fadeOut("fast","swing", function() {
-			$(this).remove();
-		});
-		updateGroupArray();
-	}
+function deleteGroup(group){
+	cs.groupNum-=1;
+	//get droparea div and put all items back to init column
+	$(group).children('.droparea').children().each(function() {
+		$('#initialColumn').append(this);
+	});
+	//remove group div
+	$(group).fadeOut("fast","swing", function() {
+		$(this).remove();
+	});
+	updateGroupArray();
+}
 
-	function createGroup(groupname, focus_on_creation = false){
-		cs.groupNum+=1;
-		var group = $("<div class='group'></div>");
-		var groupTitle = $("<div class='title' contenteditable='false'>"+groupname+"</div>");
-		var closeIcon = $("<i class='fa fa-times closeicon' aria-hidden='true'></i>");
-		var nestedArea = $("<div class='droparea nested accepts-items'></div>");
-		var grabIcon = $("<div class='iconContainer'><i class='fa fa-ellipsis-h grabicon' aria-hidden='true'></i></div>");
-		
-		groupTitle.blur(function(event){
-			//helper function for blur event
-			var title = event.target;
-			var newName = $(title).text().trim().replace(/[^\w\s]/gi, '');
-			if (cs.lastChanged != newName){
-				if (newName == ''){
-					$(title).html('Default Group');
-				} else {
-					if (!groupNameExists(newName)){
-						$(title).html(newName);
-					} else {
-						alert('group name already exists');
-						$(title).html(cs.lastChanged);
-					}
-				}
-			} 
-        	title.contentEditable = false;
-			title.classList.remove('contenteditable');
-			updateGroupArray();
-    	});
-		//if the enter key is pressed, call the blur event
-		groupTitle.keydown(function(event){
-		    if(event.keyCode == 13){
-				$(event.target).blur();
-		    }
-    	});
-
-		if (cs.studyType == 'open'){
-			group.append(closeIcon);
-			closeIcon.click(function(event){
-    	    	deleteGroup($(event.target).parent());
-    		});
-
-			groupTitle.click(function(event){
-				var title = event.target;
-	        	cs.lastChanged = $(title).text();
-	        	title.contentEditable = true;
-				title.classList.add('contenteditable')
-				this.focus();
-				document.execCommand('selectAll', false, null);
-	    	});
-		}
-    	group.append(groupTitle);
-    	group.append(nestedArea);
-    	group.append(grabIcon);
-		$('#dropZone'+((cs.zoneNum-1)+cs.groupNum)%cs.zoneNum).append(group)
-		
-		if (focus_on_creation) {
-			group.fadeIn();
-			groupTitle.click()	
-		}
-		updateContainers();
-		updateGroupArray();
-	}
-
-	function createCard(cardName){
-		$('#initialColumn').append('<div class="item">'+cardName+'</div>');
-		updateContainers()
-	}
-
-	function getResults(){
-		var results = []
-		$('.group').each(function(index) {
-			var title = $(this).children('.title')
-			var groupname = $(title).text();
-			var nestedArea = $(this).children('.droparea')
-			var cards = []
-			$(nestedArea).children().each(function(){
-				var cardname = $(this).text();
-				cards.push(cardname);
-			});
-			results.push({groupname,cards})
-		});
-		return results;
-	}
-
-	function loadDatafromDB(){
-		cs.studyType = $('#hiddenType').val();
-		cs.status = $('#hiddenActive').val();
-		cs.responseID = $('#resid').val();
-
-		if(cs.studyType == 'closed'){
-			var groups = $('#hiddenGroups').val().split(";").map(function(item) {
-			  return item.trim();
-			});
-			for (var i = 0; i < groups.length; i++) {
-				if (groups[i] != ''){
-					createGroup(groups[i]);	
-				}
-			}
-		}
-
-		var cards = $('#hiddenCards').val().split(";").map(function(item) {
-			  return item.trim();
-		});
-		for (var i = 0; i < cards.length; i++) {
-			if (cards[i] != ''){
-				createCard(cards[i]);
-			}
-		}
-
-		//dragula event to check for empty intial list on 'drop' actions
-		drake.on("drop", function(event){
-			$('#hiddenResults').val(JSON.stringify(getResults()));
-			if (($('#initialColumn').children().length == 1)){
-				$('#done').show();
+function createGroup(groupname, focus_on_creation = false){
+	cs.groupNum+=1;
+	var group = $("<div class='group'></div>");
+	var groupTitle = $("<div class='title' contenteditable='false'>"+groupname+"</div>");
+	var closeIcon = $("<i class='fa fa-times closeicon' aria-hidden='true'></i>");
+	var nestedArea = $("<div class='droparea nested accepts-items'></div>");
+	var grabIcon = $("<div class='iconContainer'><i class='fa fa-ellipsis-h grabicon' aria-hidden='true'></i></div>");
+	
+	groupTitle.blur(function(event){
+		//helper function for blur event
+		var title = event.target;
+		var newName = $(title).text().trim().replace(/[^\w\s]/gi, '');
+		if (cs.lastChanged != newName){
+			if (newName == ''){
+				$(title).html('Default Group');
 			} else {
-				$('#done').hide();
+				if (!groupNameExists(newName)){
+					$(title).html(newName);
+				} else {
+					alert('group name already exists');
+					$(title).html(cs.lastChanged);
+				}
 			}
+		} 
+    	title.contentEditable = false;
+		title.classList.remove('contenteditable');
+		updateGroupArray();
+	});
+	//if the enter key is pressed, call the blur event
+	groupTitle.keydown(function(event){
+	    if(event.keyCode == 13){
+			$(event.target).blur();
+	    }
+	});
+
+	if (cs.studyType == 'open'){
+		group.append(closeIcon);
+		closeIcon.click(function(event){
+	    	deleteGroup($(event.target).parent());
 		});
 
-		$('#newGroupButton').click(function() {
-			for (var i = 1; i < 1000; i++) {
-			var newName = "Group "+i;
-			if (!groupNameExists(newName)){
-					createGroup(newName, true);
-					break;
-				} 
-			}
-		});
-
-		$('#done').hide();
-		$('#hiddenGroups').remove();
-		$('#hiddenCards').remove();
-		$('#hiddenType').remove();
-		$('#hiddenActive').remove();
+		groupTitle.click(function(event){
+			var title = event.target;
+        	cs.lastChanged = $(title).text();
+        	title.contentEditable = true;
+			title.classList.add('contenteditable')
+			this.focus();
+			document.execCommand('selectAll', false, null);
+    	});
 	}
-	loadDatafromDB();
-});
+	group.append(groupTitle);
+	group.append(nestedArea);
+	group.append(grabIcon);
+	$('#dropZone'+((cs.zoneNum-1)+cs.groupNum)%cs.zoneNum).append(group)
+	
+	if (focus_on_creation) {
+		group.fadeIn();
+		groupTitle.click()	
+	}
+	updateContainers();
+	updateGroupArray();
+}
+
+function createCard(cardName){
+	$('#initialColumn').append('<div class="item">'+cardName+'</div>');
+	updateContainers()
+}
+
+function getResults(){
+	var results = []
+	$('.group').each(function(index) {
+		var title = $(this).children('.title')
+		var groupname = $(title).text();
+		var nestedArea = $(this).children('.droparea')
+		var cards = []
+		$(nestedArea).children().each(function(){
+			var cardname = $(this).text();
+			cards.push(cardname);
+		});
+		results.push({groupname,cards})
+	});
+	return results;
+}
+
+function setup(input_studyType,input_status,input_responseID,input_cards,input_groups){
+	drake = initDrake();
+	cs.studyType = input_studyType;
+	cs.status = input_status;
+	cs.responseID = input_responseID;
+
+	if(cs.studyType == 'closed'){
+		var groups = input_groups.split(";").map(function(item) {
+		  return item.trim();
+		});
+		for (var i = 0; i < groups.length; i++) {
+			if (groups[i] != ''){
+				createGroup(groups[i]);	
+			}
+		}
+	}
+
+	var cards = input_cards.split(";").map(function(item) {
+		  return item.trim();
+	});
+	for (var i = 0; i < cards.length; i++) {
+		if (cards[i] != ''){
+			createCard(cards[i]);
+		}
+	}
+
+	//dragula event to check for empty intial list on 'drop' actions
+	drake.on("drop", function(event){
+		$('#hiddenResults').val(JSON.stringify(getResults()));
+		if (($('#initialColumn').children().length == 1)){
+			$('#done').show();
+		} else {
+			$('#done').hide();
+
+		}
+	});
+	$('#newGroupButton').click(function() {
+		for (var i = 1; i < 1000; i++) {
+		var newName = "Group "+i;
+		if (!groupNameExists(newName)){
+				createGroup(newName, true);
+				break;
+			} 
+		}
+	});
+
+	$('#done').hide();
+}
+
--- js/productreactioncards.js
+++ js/productreactioncards.js
@@ -1,114 +1,99 @@
-$(document).ready(function() {
-	var pc = {
-		secondSelectionLimit: 5,
-		firstSelectionLimit: 10,
-		onStageTwo: false,
-		selectedNum: 0,
+var pc = {
+	secondSelectionLimit: 5,
+	firstSelectionLimit: 10,
+	onStageTwo: false,
+	selectedNum: 0,
+}
+
+function setup(input_words,input_randomize){
+	disableButton('#nextBtn');
+	disableButton('#done');
+	$('#done').hide();	
+	pc.words = input_words.split(";").map(function(item) {
+		  return item.trim();
+	}).filter(function(n) { return n != ''});
+	//shuffle the word order if randomize is checked
+	if (input_randomize == "on") {
+		pc.words = shuffle(pc.words)
 	}
-
-	function shuffle(array) {
-	  var currentIndex = array.length, temporaryValue, randomIndex;
-	  // While there remain elements to shuffle...
-	  while (0 !== currentIndex) {
-	    // Pick a remaining element...
-	    randomIndex = Math.floor(Math.random() * currentIndex);
-	    currentIndex -= 1;
-	    // And swap it with the current element.
-	    temporaryValue = array[currentIndex];
-	    array[currentIndex] = array[randomIndex];
-	    array[randomIndex] = temporaryValue;
-	  }
-	  return array;
+	//add words to DOM
+	for (var i = 0; i < pc.words.length; i++) {
+		$('#cardArea').append("<li>"+pc.words[i]+"</li>");
 	}
-
-	function loadDatafromDB(){
-		pc.words = $('#hiddenWords').val().split(";").map(function(item) {
-			  return item.trim();
-		}).filter(function(n) { return n != ''});
-
-		if ($('#hiddenRandomize').val() == "on") {
-			pc.words = shuffle(pc.words)
-		}
-		
-		for (var i = 0; i < pc.words.length; i++) {
-			$('#cardArea').append("<li>"+pc.words[i]+"</li>");
-		}
-
-		$("#done").click(function() {
-			if ($("#cardArea li.selected").length >= pc.secondSelectionLimit) {
-				pc.results = [];
-				$('#cardArea li.selected').each(function(idx,word){
-					pc.results.push($(word).text());
-				});
-				$('#hiddenResults').val(JSON.stringify(pc.results));
-				$('#submitForm').click();
-			}
-		});	
-
-		$('#hiddenWords').remove();
-		$('#hiddenStatus').remove();
-		$('#hiddenRandomize').remove();
-	}
-
-	function enableButton(buttonID){
-		$(buttonID).removeClass('disabled');
-		$(buttonID).addClass('btn-amber');
-	}
-
-	function disableButton(buttonID){
-		$(buttonID).addClass('disabled');
-		$(buttonID).removeClass('btn-amber');
-	}
-
-	function setInstructions(str){
-		$('#textArea').html(str);
-	}
-
-	function updateView(){
-		pc.selectedNum = $('#cardArea li.selected').length;
-		if (pc.onStageTwo) {
-			if(pc.selectedNum == pc.secondSelectionLimit){
-				$( "#cardArea li:not(.selected)").toggleClass('disabled');
-				enableButton('#done');
-			} else {
-				$( "#cardArea li").removeClass('disabled');
-				setInstructions('Select your top 5 cards.');
-				disableButton('#done');
-			}
-		} else {
-			setInstructions('Select at least 10 cards that represent your experience with the system. ('+pc.selectedNum+' selected)');
-			if(pc.selectedNum >= pc.firstSelectionLimit){
-				enableButton('#nextBtn');
-			} else {
-				disableButton('#nextBtn');
-			}
-		}
-	}
-	
-	$("#nextBtn").click(function() {
-  		if ($("#cardArea li.selected").length >= pc.firstSelectionLimit) {
-				$( "#cardArea li:not(.selected)").each(function(index) {
-				$(this).remove();
+	//bind done action to submit study result
+	$("#done").click(function() {
+		if ($("#cardArea li.selected").length >= pc.secondSelectionLimit) {
+			pc.results = [];
+			$('#cardArea li.selected').each(function(idx,word){
+				pc.results.push($(word).text());
 			});
-			$("#cardArea li.selected").toggleClass('selected');
-			pc.onStageTwo = true;
-			updateView();
-			$('#done').show();
+			$('#hiddenResults').val(JSON.stringify(pc.results));
+			$('#submitForm').click();
+		}
+	});	
+	//bind next button action function
+	$("#nextBtn").click(function() {
+		if ($("#cardArea li.selected").length >= pc.firstSelectionLimit) {
+			$( "#cardArea li:not(.selected)").each(function(index) {
 			$(this).remove();
-  		}		
+		});
+		$("#cardArea li.selected").toggleClass('selected');
+		pc.onStageTwo = true;
+		updateView();
+		$('#done').show();
+		$(this).remove();
+		}		
 	});
-
-	function init(){
-		disableButton('#nextBtn');
-		disableButton('#done');
-		$('#done').hide();
-		loadDatafromDB();
-	}	
-	init()
-
+	//bind card selection function
 	$("#cardArea li").click(function() {
 		$(this).toggleClass("selected");
 		updateView();
 	});
-});
+}
 
+function enableButton(buttonID){
+	$(buttonID).removeClass('disabled');
+	$(buttonID).addClass('btn-amber');
+}
+
+function disableButton(buttonID){
+	$(buttonID).addClass('disabled');
+	$(buttonID).removeClass('btn-amber');
+}
+
+function setInstructions(str){
+	$('#textArea').html(str);
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function updateView(){
+	pc.selectedNum = $('#cardArea li.selected').length;
+	if (pc.onStageTwo) {
+		if(pc.selectedNum == pc.secondSelectionLimit){
+			$( "#cardArea li:not(.selected)").toggleClass('disabled');
+			enableButton('#done');
+		} else {
+			$( "#cardArea li").removeClass('disabled');
+			setInstructions('Select your top 5 cards.');
+			disableButton('#done');
+		}
+	} else {
+		setInstructions('Select at least 10 cards that represent your experience with the system. ('+pc.selectedNum+' selected)');
+		if(pc.selectedNum >= pc.firstSelectionLimit){
+			enableButton('#nextBtn');
+		} else {
+			disableButton('#nextBtn');
+		}
+	}
+}
